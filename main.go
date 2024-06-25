390b40194115a34bf9a631e459880ecea9c45e2d
Kesit Budi Kusumo <kesit@kernel.id>
2024-06-25 22:05:08 +0700

    feat: add iterative solution

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"slices"
+)
 
+func main() {
+	input := []int{1, 4, 3, 2}
+	output := arrayPairSum(input)
+	println(output)
+}
+
+func arrayPairSum(nums []int) int {
+	slices.Sort(nums)
+	result := 0
+	for i := 0; i < len(nums); i += 2 {
+		result += nums[i]
+	}
+	return result
 }
